12060c14e06cb2a5c49ad8c4e41c6f7bf7558a88
sarnet43 <158814006+sarnet43@users.noreply.github.com>
2024-04-27 13:01:45 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,12 +1,10 @@
 class Solution {
     public int[] solution(int money) {
         int[] answer = new int[2];
-        int cnt = 0;
         while(money>=5500){
             money-= 5500;
-            cnt++;
+            answer[0]++;
         }
-        answer[0] = cnt;
         answer[1] = money;
         return answer;
     }
