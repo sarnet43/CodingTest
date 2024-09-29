466f55192df37321b054159db34b1474db35054c
이수아 <158814006+sarnet43@users.noreply.github.com>
2024-09-29 21:08:56 +0900

    [level 0] Title: 꼬리 문자열, Time: 2.36 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String[] str_list, String ex) {
+        String answer = "";
+        for(String str : str_list){
+            if(str.contains(ex) != true)
+                answer += str;
+        }
+        return answer;
+    }
+}
