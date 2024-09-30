d6423baac30418d0c36e3e64e75d557c4fa73ed4
이수아 <158814006+sarnet43@users.noreply.github.com>
2024-09-30 21:48:25 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.04 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int add = 0, even = 0;
+        for(int i = 0; i < num_list.length; i++){
+            if(i % 2 == 0) add += num_list[i];
+            else even += num_list[i];
+        }
+        return add >= even ? add : even;
+    }
+}
