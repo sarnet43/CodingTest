365ae8c45512967f9f7fc4676c1a5bd73baeef35
이수아 <158814006+sarnet43@users.noreply.github.com>
2025-03-01 19:38:46 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 91.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        int check[] = {0,0,0,0,0,0,0,0,0,0};
+        for (int num : numbers){
+            check[num] = 1;
+        }
+        for(int i = 0; i < check.length; i++){
+            if(check[i] == 0) answer += i;
+        }
+        return answer;
+    }
+}
